e71e3b43ca7dd5d5281f2ea04f79345ecff984cd
muhammed-swalih <swalihnaduthodi4@gmail.com>
2023-05-10 12:12:35 +0530

    one more change for endpoint

--- routes/packages.js
+++ routes/packages.js
@@ -17,7 +17,7 @@ router.post('/postpackages', postHoneyMoon)
 
 router.post('/ads', postAd)
 
-router.get('getpackages', getHoneymoon)
+router.get('/getpackages', getHoneymoon)
 
 router.get('/getAd', getAd)
 
